546b8aa7b325ab8ee4734eb9c07ed41c7c1052a8
ctdurazo <ctdurazo@gmail.com>
2020-04-24 22:57:35 -0700

    creating inline_calculator method that will handle a list of numbers and operator input. Change main calculator method to call this mehtod on string input, and now will print "invalid input try again" from the inline_calculator method.

--- rpn.py
+++ rpn.py
@@ -36,8 +36,7 @@ def calculator():
                     elif str(user_input) == "q":
                         raise EOFError
                     else:
-                        print("invalid input try again")
-                        out = None
+                        out = inline_calculator(list(user_input.split(" ")), nums)
                     if out is not None:
                         nums.append(out)
                         print(out)
@@ -46,6 +45,42 @@ def calculator():
             user_input = input("> ")
     except EOFError:
         pass
+
+
+def inline_calculator(input_list, nums):
+    out = None
+    for user_input in input_list:
+        try:
+            val = int(user_input)
+            nums.append(val)
+        except ValueError:
+            try:
+                val = float(user_input)
+                nums.append(val)
+            except ValueError:
+                if str(user_input) == "+" and len(nums) > 1:
+                    b = nums.pop()
+                    a = nums.pop()
+                    out = a + b
+                elif str(user_input) == "-" and len(nums) > 1:
+                    b = nums.pop()
+                    a = nums.pop()
+                    out = a - b
+                elif str(user_input) == "*" and len(nums) > 1:
+                    b = nums.pop()
+                    a = nums.pop()
+                    out = a * b
+                elif str(user_input) == "/" and len(nums) > 1:
+                    b = nums.pop()
+                    a = nums.pop()
+                    out = a / b
+                elif str(user_input) == "q":
+                    raise EOFError
+                else:
+                    print("invalid input try again")
+                if out is not None:
+                    nums.append(out)
+    return out
 
 
 if __name__ == "__main__":
